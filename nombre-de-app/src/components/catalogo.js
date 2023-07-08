1ddfff11cf7430c9a0ff9b300912d97d2bed89e5
Juli Rodriguez <julietaa.rodriguezz3@gmail.com>
2023-07-07 21:39:45 -0300

    segunda entrega

--- nombre-de-app/src/components/catalogo.js
+++ nombre-de-app/src/components/catalogo.js
@@ -1,32 +1,42 @@
-import React, { useState, useEffect} from 'react';
-import Product from '../product';
-import productsData from './products.json'
+import React, { useState, useEffect } from 'react';
+import productsData from './products.json';
 
 function Catalogo() {
-    const [products, setProducts] = useState([]);
+const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setProducts(productsData);
-        };
-        fetchData();
-    }, []);
+useEffect(() => {
+    const fetchData = async () => {
+    setProducts(productsData);
+    };
+    fetchData();
+}, []);
 
-    const handleAddToCart = (productId) => {
+const Product = ({ id, name, stock, onAddToCart }) => {
+    // Implementación del componente Product
+    return (
+    <div>
+        <h3>{name}</h3>
+        <p>Stock: {stock}</p>
+        <button onClick={() => onAddToCart(id)}>Agregar al carrito</button>
+    </div>
+    );
+};
+
+const handleAddToCart = (productId) => {
     setProducts((prevProducts) =>
     prevProducts.map((product) => {
-        if (product.id === productId && product.stock > 0 ) {
-        return { ...product, stock: product.stock - 1};
-    }
-    return product;
+        if (product.id === productId && product.stock > 0) {
+        return { ...product, stock: product.stock - 1 };
+        }
+        return product;
     })
     );
 };
 
-    return (
+return (
     <div className="App">
-        <h2>Catalogo</h2>
-        {products.map((product) => (
+    <h2>Catalogo</h2>
+    {products.map((product) => (
         <Product
         key={product.id}
         id={product.id}
@@ -34,9 +44,9 @@ function Catalogo() {
         stock={product.stock}
         onAddToCart={handleAddToCart}
         />
-        ))}
+    ))}
     </div>
-    );
+);
 }
 
 export default Catalogo;
